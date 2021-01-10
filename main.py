f45144369bafb4b92ff76fc16f68187dfaf9084f
Marcel Pinto <p.marcel.augusto@gmail.com>
2021-01-10 20:32:15 -0300

    added poisson small world graph generator

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 from random import choices, sample
+from poisson_small_world_network import poisson_small_world_graph
+from sir import sir_model_dynamics
+import numpy as np
 
 
 class Epidemic_Network:
@@ -13,45 +16,56 @@ class Epidemic_Network:
     }
     G: nx.Graph
     cumulative_cases = []
-    daily_cases = [0]
+    daily_cases = []
+
+    D = None
+    k = None
+    p = None
 
     def __init__(self,
-                 number_of_nodes,
-                 k,
                  infection_rate,
-                 p,
                  days_until_recovery,
                  number_of_initial_infected,
                  save_steps=False
                  ):
-        self.number_of_nodes = number_of_nodes
-        self.k = k
         self.infection_rate = infection_rate
-        self.p = p
         self.days_until_recovery = days_until_recovery
         self.number_of_initial_infected = number_of_initial_infected
-        self.generate_epidemic_network(self.number_of_nodes, self.k, self.p)
+        self.daily_cases.append(number_of_initial_infected)
+        self.save_steps = save_steps
 
-        num_edges = self.G.number_of_edges()
-        num_nodes = self.G.number_of_nodes()
-        average_degree = num_edges / num_nodes
-        print(f"Average degree: {average_degree}")
+    def generate_epidemic_network(self, network_generation_method, **kwargs) -> nx.Graph:
+        self.G = network_generation_method(**kwargs)
 
+        if "D" in kwargs.keys():
+            self.D = kwargs["D"]
+        if "k" in kwargs.keys():
+            self.k = kwargs["k"]
+        self.p = kwargs["p"]
+
+        self.network_gen_method = network_generation_method.__name__
+        self.network_gen_parameters = str(kwargs)
+        nx.set_node_attributes(self.G, values="susceptible", name="status")
+        return self
+
+    def simulate(self):
         self.distribute_initial_infection(self.number_of_initial_infected)
         day = 1
-        if save_steps:
+        if self.save_steps:
             plt.figure(figsize=(5, 5))
         while True:
-            infected = self.get_by_status("infected")
-            self.interact(day, save_steps)
+            # infected = self.get_by_status("infected")
+            self.interact(day, self.save_steps)
             self.count_cumulative_cases()
             self.count_daily_cases()
             self.update_disease_progress()
             day += 1
-            if not infected:
+            if day > 100:
                 break
+                # if not infected:
+                #     break
         plt.close()
-        self.plot_cases_curves()
+        self.plot_cases()
 
     def interact(self, day, save_steps=False):
         infected = self.get_by_status("infected")
@@ -60,7 +74,7 @@ class Epidemic_Network:
                         person2 in infected_person_contacts
                         if self.G.nodes[person2]["status"] == "susceptible")
 
-        for susceptible_person in set(susceptibles):
+        for susceptible_person in susceptibles:
             if self.infection_occurred():
                 nx.set_node_attributes(
                     self.G, values={susceptible_person: self.infected})
@@ -80,10 +94,6 @@ class Epidemic_Network:
                             attributes in self.G.nodes(data=True)
                             if attributes["status"] == "infected"}
         nx.set_node_attributes(self.G, values=updated_infected)
-
-    def generate_epidemic_network(self, N: int, k: int, p: float) -> nx.Graph:
-        self.G = nx.watts_strogatz_graph(N, k, p)
-        nx.set_node_attributes(self.G, values="susceptible", name="status")
 
     def infection_occurred(self):
         return choices([True, False],
@@ -112,19 +122,41 @@ class Epidemic_Network:
                            ])
         self.daily_cases.append(today_cases)
 
-    def plot_cases_curves(self):
-        _, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True)
+    def plot_cases(self):
+        _, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True, dpi=300)
 
         ax1.set_ylabel("Cumulative cases")
         ax2.set_ylabel("Daily cases")
         ax2.set_xlabel("Days")
-        ax1.set_xlim(0, len(self.daily_cases))
-        ax1.plot(self.cumulative_cases, color="black")
-        ax2.plot(self.daily_cases, color="black")
-        plt.savefig(
-            f"cases_vs_days_N={self.number_of_nodes}"
-            f"_k={self.k}_p={self.p}_d={self.days_until_recovery}"
-            f"_r={self.infection_rate}.png")
+        # ax1.set_xlim(-0.1, len(self.daily_cases))
+        ax1.plot(self.cumulative_cases, color="black", label="Network")
+        # ax2.plot(self.daily_cases, 'black', marker="o")
+        ax2.bar(list(range(len(self.daily_cases))),
+                self.daily_cases, color="red", edgecolor="black")
+        if self.D:
+            ax1.set_title(
+                f"N={self.G.number_of_nodes()}, D={self.D}, r={self.infection_rate}, $\\epsilon={self.p}$")
+            beta = self.infection_rate * self.D
+        if self.k:
+            ax1.set_title(
+                f"N={self.G.number_of_nodes()}, k={self.k}, r={self.infection_rate}, $\\epsilon={self.p}$")
+            beta = self.infection_rate * self.k
+
+        gamma = 1/self.days_until_recovery
+        t = np.linspace(0, 100, 100)
+        _, I, R = sir_model_dynamics(
+            N=self.G.number_of_nodes(),
+            I0=self.number_of_initial_infected,
+            beta=beta,
+            gamma=gamma,
+            t=t
+        )
+        ax1.plot(R + I, 'g', label="Recovered + Infected (SIR)")
+        ax1.legend()
+        sulfix = self.parse_output_file_name_sulfix()
+        filename = f"cases_vs_days_{sulfix}.png"
+        plt.savefig(filename)
+        plt.clf()
 
     def draw(self, day):
         plt.title(f"Day: {day}")
@@ -146,13 +178,24 @@ class Epidemic_Network:
         plt.savefig(f"{day}-evolution.png")
         plt.clf()
 
+    def parse_output_file_name_sulfix(self):
+        self.network_gen_parameters = self.network_gen_parameters.replace(
+            "{", "").replace("}", "").replace(", ", "_") \
+            .replace(": ", "=").replace("'", "")
+        return f"type={self.network_gen_method}_{self.network_gen_parameters}_r={self.infection_rate}_d={self.days_until_recovery}_i0={self.number_of_initial_infected}"
+
+
+# Epidemic_Network(
+#     infection_rate=0.1,
+#     days_until_recovery=6,
+#     number_of_initial_infected=10,
+# ).generate_epidemic_network(
+#     nx.watts_strogatz_graph, n=1000, k=10, p=0.1).simulate()
+
 
 Epidemic_Network(
-    number_of_nodes=100,
-    k=8,
     infection_rate=0.1,
-    p=0.4,
     days_until_recovery=6,
     number_of_initial_infected=10,
-    save_steps=True
-)
+).generate_epidemic_network(
+    poisson_small_world_graph, n=1000, D=3, p=0.1).simulate()
---
+++ poisson_small_world_network.py
@@ -0,0 +1,99 @@
+import networkx as nx
+import numpy as np
+from math import ceil
+from random import choices, choice
+from typing import Tuple, List
+from itertools import chain
+
+
+def __get_nearest_neighbors_connections(node: int,
+                                        num_connections: int,
+                                        number_of_nodes: int) -> List[Tuple[int, int]]:
+    connections_to_right = ceil(num_connections / 2)
+    connections_to_left = num_connections - connections_to_right
+
+    right_connections = np.array(
+        [i for i in range(node + 1, node + connections_to_right + 1)])
+    right_connections[right_connections >
+                      number_of_nodes - 1] -= number_of_nodes
+
+    left_connections = np.array(
+        [i for i in range(node - connections_to_left, node)])
+    left_connections[left_connections < 0] += number_of_nodes
+
+    neighbor_to_connect = np.concatenate([right_connections, left_connections])
+
+    return [sorted([node, neighbor]) for neighbor in neighbor_to_connect]
+
+
+def __is_rewired(probability: float) -> bool:
+    return choices([True, False], weights=[probability, 1 - probability])[0]
+
+
+def __rewire_connection(node_connection: Tuple[int, int], probability: float, number_of_nodes: int):
+    if __is_rewired(probability):
+        nodes = list(range(number_of_nodes))
+        node, _ = node_connection
+        # A node cannot rewire with itself
+        nodes.remove(node)
+        rewired_neighbor = choice(nodes)
+        return (node, rewired_neighbor)
+
+    return node_connection
+
+
+def __create_edges(number_of_nodes: int, lam: int, rewiring_probability: float):
+    if rewiring_probability < 0:
+        raise ValueError("Rewiring probability must be greater than 0")
+
+    connectivity_distribution = np.random.poisson(
+        lam=lam, size=number_of_nodes)
+
+    connectivity = [__get_nearest_neighbors_connections(node, connections, number_of_nodes)
+                    for node, connections in enumerate(connectivity_distribution)]
+
+    connectivity_without_repeated_edges = {tuple(item)
+                                           for item in chain.from_iterable(connectivity)}
+    if rewiring_probability > 0:
+        rewired_connectivity = [
+            __rewire_connection(
+                connection, rewiring_probability, number_of_nodes)
+            for connection in connectivity_without_repeated_edges]
+        return rewired_connectivity
+
+    return list(connectivity_without_repeated_edges)
+
+
+def poisson_small_world_graph(n: int, D: int, p: float) -> nx.Graph:
+    """Returns a Poisson small-world graph.
+
+    Parameters
+    ----------
+    n : int
+        The number of nodes
+    D : int
+        The average degree of the network. It is the same lambda of poisson
+        distribution
+
+    p : float
+        The probability of rewiring each edge
+
+    """
+    G = nx.Graph()
+    G.add_nodes_from(list(range(n)))
+    edges = __create_edges(n, D, p)
+    G.add_edges_from(edges)
+
+    return G
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    def __draw_circular(graph):
+        pos = nx.circular_layout(graph)
+        nx.draw_networkx(graph, pos=pos)
+        plt.show()
+
+    H = poisson_small_world_graph(50, 8, .5)
+    __draw_circular(H)
